2d94825179d5baaa70f19e3fe88a5bb8c6bbb985
Olga Liashenko <liashen.olga@gmail.com>
2018-11-18 00:40:17 +0100

    add implementation for reverseString

--- src/main/java/liashenko/MyStringUtils.java
+++ src/main/java/liashenko/MyStringUtils.java
@@ -3,6 +3,13 @@ package liashenko;
 public class MyStringUtils implements StringUtilInterface {
 
     public String reverseString(String input) {
-        return null;
+        char[] inputChars = input.toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = inputChars.length - 1; i >= 0; i--) {
+            result.append(inputChars[i]);
+        }
+
+        return result.toString();
     }
 }
--- src/test/java/MyStringUtilsTest.java
+++ src/test/java/MyStringUtilsTest.java
@@ -1,16 +1,39 @@
 import liashenko.MyStringUtils;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 
 public class MyStringUtilsTest {
 
-    @Test
-    public void testReverseString(){
-        System.out.println("Test start!");
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Test suit started!");
+    }
 
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Test suit finished!");
+    }
+
+    @Before
+    public void before() {
+        System.out.println("Test started!");
+    }
+
+    @After
+    public void after() {
+        System.out.println("Test finished.");
+    }
+
+    @Test
+    public void testReverseStringPositive() {
         MyStringUtils myStringUtils = new MyStringUtils();
         String actual = myStringUtils.reverseString("123");
 
         Assert.assertEquals("321", actual);
+    }
+
+    @Test
+    public void testReverseStringWithNull() {
+        MyStringUtils myStringUtils = new MyStringUtils();
+        myStringUtils.reverseString(null);
     }
 }
